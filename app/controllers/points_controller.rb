3f490a0608bf54b3cb64c63098fe9dadc2b7ad5e
Takuya Arita <takuya.arita@gmail.com>
2014-06-25 21:20:04 +0900

    不要なアクションなどを削除

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,26 +1,5 @@
 class PointsController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_action :set_point, only: [:show, :edit, :update, :destroy]
-
-  # GET /points
-  # GET /points.json
-  def index
-    @points = Point.all
-  end
-
-  # GET /points/1
-  # GET /points/1.json
-  def show
-  end
-
-  # GET /points/new
-  def new
-    @point = Point.new
-  end
-
-  # GET /points/1/edit
-  def edit
-  end
 
   # POST /points
   # POST /points.json
@@ -29,36 +8,10 @@ class PointsController < ApplicationController
 
     respond_to do |format|
       if @point.save
-        format.html { redirect_to @point, notice: 'Point was successfully created.' }
         format.json { render :show, status: :created, location: @point }
       else
-        format.html { render :new }
         format.json { render json: @point.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /points/1
-  # PATCH/PUT /points/1.json
-  def update
-    respond_to do |format|
-      if @point.update(point_params)
-        format.html { redirect_to @point, notice: 'Point was successfully updated.' }
-        format.json { render :show, status: :ok, location: @point }
-      else
-        format.html { render :edit }
-        format.json { render json: @point.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /points/1
-  # DELETE /points/1.json
-  def destroy
-    @point.destroy
-    respond_to do |format|
-      format.html { redirect_to points_url, notice: 'Point was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -66,7 +19,7 @@ class PointsController < ApplicationController
     sum = Point.group(:x, :y).select("x, y, COUNT(*) count")
     render :json => sum
   end
-  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_point
